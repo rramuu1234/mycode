d27a6a0f58b4a300194276a46d1d9a32a7794902
venkatesh <venkatesh@qwinix.com>
2017-04-18 16:18:12 +0530

    resolves the sampleLight

---
+++ sample.groovy
@@ -0,0 +1,53 @@
+
+
+include 'asynchttp_v1'
+/**
+ *  MyNewLight
+ *
+ *  Copyright 2017 venkatesh
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "MyNewLight",
+    namespace: "NewLight",
+    author: "venkatesh",
+    description: "Sample Light",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("Title") {
+        input "lock", "capability.lock", multiple: false 
+        input "switch", "capability.switch", multiple: false
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+     log.info "subscribing to events"
+     subscribe(lock, "lock", switchLevelHandler)
+    // TODO: subscribe to attributes, devices, locations, etc.
+}
+
